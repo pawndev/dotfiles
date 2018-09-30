2e9bf8058ffacbe3d2a5a362bfbd7e0fd3261066
pawndev <coquelet.c@gmail.com>
2018-09-30 14:54:56 +0200

    Add some bindings

--- i3/install.zsh
+++ i3/install.zsh
@@ -5,17 +5,20 @@ DIRNAME="$(cd "$(dirname "$0")";pwd -P)"
 
 if [[ `uname` == 'Linux' ]]; then
   echo "We're on linux"
-  sudo pacman -S --needed --noconfirm i3 libmpdclient
-  yaourt -S --needed --noconfirm polybar
+  sudo pacman -S --needed --noconfirm i3 libmpdclient feh scrot xclip
+  yaourt -S --needed --noconfirm polybar light
 
   echo "Creating i3 config directory"
   mkdir -p ~/.config/i3/scripts
   echo "Creating polybar config directory"
   mkdir -p ~/.config/polybar
+  mkdir -p ~/Pictures/Screenshots
 
   echo "Linking i3 conf"
   ln -sf $DIRNAME/config ~/.config/i3/
   ln -sf $DIRNAME/scripts/i3exit ~/.config/i3/scripts/
+  ln -sf $DIRNAME/scripts/random-wallpaper ~/.config/i3/scripts/
+  ln -sf $DIRNAME/scripts/take-screenshot ~/.config/i3/scripts/
   ln -sf $DIRNAME/polybar/config ~/.config/polybar/
   ln -sf $DIRNAME/polybar/launch.sh ~/.config/polybar/
 fi
